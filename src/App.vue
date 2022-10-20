eafa3d85a69f1a658f20dfa66351ef4560b630c7
Hosea Patricia Corpuz <Hosea.Corpuz@EntainGroup.com>
2022-10-20 19:28:33 +0800

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <div >
-    <rating-form></rating-form>
-  </div>
+    <div class="flex-grow my-60">
+      <rating-form></rating-form>
+    </div>
+    <footer class="text-gray-400 justify-center">
+        Challenge by 
+        <a href="https://www.frontendmentor.io/" class="text-orange-500  hover:text-orange-200 active:text-orange-200"> Frontend Mentor</a>
+        Coded by 
+        <a href="https://github.com/seya101" class="text-orange-500  hover:text-orange-200 active:text-orange-200 "> Hosea Patricia Corpuz</a>
+    </footer>
 </template>
 
 <script setup>
